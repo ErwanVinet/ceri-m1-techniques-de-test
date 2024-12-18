9d4be51e10b54fd45c775068d19ba1f9429b6387
Erwan <erwanvinet034@gmail.com>
2024-12-18 14:04:42 +0100

    changement IPokemonMetadataProviderTest

--- src/main/java/fr/univavignon/pokedex/api/PokemonMetadataProvider.java
+++ src/main/java/fr/univavignon/pokedex/api/PokemonMetadataProvider.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Objects;
+import java.util.HashMap;
+import java.util.Map;
 
 public class PokemonMetadataProvider implements IPokemonMetadataProvider {
 
